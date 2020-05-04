2b980741a3821053e4da8e4677d6a5d140ffee16
yuehhan <yuehwhan@gmail.com>
2020-05-03 21:35:32 -0400

    remove gitignore

---
+++ config/db_connect.php
@@ -0,0 +1,8 @@
+<?php 
+   $conn = mysqli_connect('localhost', 'yueh', 'test1234', 'php_project1');
+
+   // check connection
+   if(!$conn){
+       echo 'Connection error: ' . mysqli_connect_error();
+   }
+?>
