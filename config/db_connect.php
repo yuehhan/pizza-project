d25cfd93bfa851b33e20f1731f7ef1e8f6e09887
yuehhan <51277751+yuehhan@users.noreply.github.com>
2020-05-03 21:36:24 -0400

    Delete db_connect.php

--- config/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   $conn = mysqli_connect('localhost', 'yueh', 'test1234', 'php_project1');
-
-   // check connection
-   if(!$conn){
-       echo 'Connection error: ' . mysqli_connect_error();
-   }
-?>
